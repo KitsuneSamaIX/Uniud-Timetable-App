21202fd610d0a87b97fa15dbe9ca2ec302e9f72c
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-03-11 02:35:56 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/mattiafedrigo/uniud_timetable_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mattiafedrigo.uniud_timetable_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/uniud_timetable_api.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart' as http;
+
+void main() {
+  print('Hello world');
+}
+
+class Course {
+  Course();
+}
+
