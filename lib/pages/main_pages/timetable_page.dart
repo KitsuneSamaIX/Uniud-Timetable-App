54bfe4c243c3a5366fe32c67cd4aa8d1d9b526f1
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-03-25 19:20:12 +0100

    code improvements

--- lib/pages/main_pages/timetable_page.dart
+++ lib/pages/main_pages/timetable_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
 
 class TimetablePage extends StatefulWidget {
@@ -8,11 +9,24 @@ class TimetablePage extends StatefulWidget {
 }
 
 class _TimetablePageState extends State<TimetablePage> {
+  var _selectedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        Expanded(
+      children: [
+        CalendarTimeline(
+          initialDate: _selectedDay,
+          firstDate: DateTime.now().subtract(const Duration(days: 365)),
+          lastDate: DateTime.now().add(const Duration(days: 365)),
+          onDateSelected: (dateTime) {}, // TODO onDateSelected callback
+          leftMargin: 60,
+          monthColor: Theme.of(context).colorScheme.secondary,
+          dayColor: Theme.of(context).colorScheme.secondary,
+          activeDayColor: Theme.of(context).colorScheme.primary,
+          activeBackgroundDayColor: Theme.of(context).colorScheme.primaryContainer,
+        ),
+        const Expanded(
           child: Center(
             child: Text(
               'Hello Fellow Student!',
