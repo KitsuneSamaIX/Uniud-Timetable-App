7feb2ee519bfdb407eae9d1f39e016074ab480c3
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-04-04 04:01:53 +0200

    Added some TODO comments.

--- lib/utilities/profiles.dart
+++ lib/utilities/profiles.dart
@@ -21,4 +21,13 @@ class Profiles extends ChangeNotifier {
     _profiles.remove(profile);
     notifyListeners();
   }
+
+// TODO add a function to get all courses from all profiles in a list, then
+//    for each course i will sort the lessons based on the DateTime of the lesson
+//    (ONLY ONE TIME SORT is needed, and maybe i could add a property to set
+//    when the lessons of a course are sorted), with the lessons sorted i will be
+//    able to use a binary search over the sorted list thus improving the search time
+//    a lot.
+//    I could also create a single list with ALL the lessons, sort that list one time,
+//    and then apply binary search on that list.
 }
