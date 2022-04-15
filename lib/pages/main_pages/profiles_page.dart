ce3aa3d00b8e0ce0aa97a273bb7b0055340b342e
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-04-15 18:38:08 +0200

    New feature: you can now slide to delete profiles.

--- lib/pages/main_pages/profiles_page.dart
+++ lib/pages/main_pages/profiles_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
+import 'package:uniud_timetable_app/models/profile_models.dart';
 import 'package:uniud_timetable_app/utilities/profiles.dart';
 
 class ProfilesPage extends StatefulWidget {
@@ -18,18 +20,8 @@ class _ProfilesPageState extends State<ProfilesPage> {
       return ListView.separated(
         itemCount: profilesProvider.profiles.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            isThreeLine: true,
-            title: Text(
-              profilesProvider.profiles[index].name,
-              style: TextStyle(color: Theme.of(context).colorScheme.primary),
-            ),
-            subtitle: Text(
-              '${profilesProvider.profiles[index].degreeName}\n'
-                  '${profilesProvider.profiles[index].degreeTypeName}, '
-                  '${profilesProvider.profiles[index].periodName}',
-              style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-            ),
+          return _ProfileTile(
+            profile: profilesProvider.profiles[index],
           );
         },
         separatorBuilder: (BuildContext context, int index) {
@@ -52,14 +44,64 @@ class _ProfilesPageState extends State<ProfilesPage> {
               children: [
                 Icon(Icons.people_alt_rounded, size: 60, color: Theme.of(context).colorScheme.secondary,),
                 const SizedBox(height: 16,),
-                const Text('Fellow student, welcome to the profile creation '
-                    'tool!\nYou can create your first profile by tapping on '
-                    'the plus icon at the bottom right of the screen.', textAlign: TextAlign.center,),
+                const Text(
+                  'Welcome to the profile creation tool!\n'
+                      'You can create a profile by tapping on the + icon at '
+                      'the bottom right of the screen.\n\n'
+                      'PRO TIP: a profile can be deleted by sliding it from'
+                      ' right to left.',
+                  textAlign: TextAlign.center,
+                ),
               ],
             ),
           ),
         ),
       );
     }
+  }
+}
+
+class _ProfileTile extends StatelessWidget {
+  final Profile profile;
+
+  const _ProfileTile({
+    Key? key, required this.profile
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      key: ValueKey(profile),
+
+      endActionPane: ActionPane(
+        extentRatio: 0.25,
+        motion: const ScrollMotion(),
+        dismissible: DismissiblePane(
+          onDismissed: () =>
+              Provider.of<Profiles>(context, listen: false).removeProfile(profile),
+        ),
+        children: [
+          SlidableAction(
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
+            label: 'Delete',
+            onPressed: (context) =>
+                Provider.of<Profiles>(context, listen: false).removeProfile(profile),
+          ),
+        ],
+      ),
+      child: ListTile(
+        isThreeLine: true,
+        title: Text(
+          profile.name,
+          style: TextStyle(color: Theme.of(context).colorScheme.primary),
+        ),
+        subtitle: Text(
+          '${profile.degreeName}\n'
+              '${profile.degreeTypeName}, ${profile.periodName}',
+          style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+        ),
+      ),
+    );
   }
 }
