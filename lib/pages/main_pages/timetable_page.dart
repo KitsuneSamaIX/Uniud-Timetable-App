dd6fb40a8d58af91c97fd1c465b00d023108b28e
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-04-19 23:12:40 +0200

    New feature: lesson info popup with Hero animation.

--- lib/pages/main_pages/timetable_page.dart
+++ lib/pages/main_pages/timetable_page.dart
@@ -101,58 +101,185 @@ class _LessonCard extends StatelessWidget {
     final endTimeString = '${lesson.endDateTime.hour}:'
         '${lesson.endDateTime.minute}';
 
-    return Card(
-      color: Theme.of(context).colorScheme.secondaryContainer,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
-      child: SizedBox(
-        height: 120,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          child: IconTheme(
-            data: IconThemeData(
-              size: 18,
-              color: Theme.of(context).colorScheme.tertiary,
-            ),
-            child: DefaultTextStyle(
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onSecondaryContainer,
-                fontSize: 14.5,
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () =>
+          Navigator.of(context).push(_HeroDialogRoute<void>(
+                  (context) => _LessonInfoDialog(lesson: lesson))),
+      child: Hero(
+        tag: lesson,
+        child: Card(
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+          child: SizedBox(
+            height: 120,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              child: IconTheme(
+                data: IconThemeData(
+                  size: 18,
+                  color: Theme.of(context).colorScheme.tertiary,
+                ),
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    fontSize: 14.5,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(lesson.course!.name + '\n',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 2,
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          const Icon(Icons.access_time_rounded),
+                          const SizedBox(width: 8),
+                          Text('$startTimeString '),
+                          Icon(Icons.arrow_forward_rounded,
+                            color: Theme.of(context).colorScheme.onSecondaryContainer,
+                          ),
+                          Text(' $endTimeString'),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          const Icon(Icons.place_outlined),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: Text('${lesson.room}, ${lesson.building}'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(lesson.course!.name + '\n',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _HeroDialogRoute<T> extends PageRoute<T> {
+  final WidgetBuilder builder;
+
+  _HeroDialogRoute(this.builder) : super();
+
+  @override
+  bool get opaque => false;
+
+  @override
+  Color? get barrierColor => Colors.black54;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  String? get barrierLabel => 'Lesson Info Barrier';
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return builder(context);
+  }
+}
+
+class _LessonInfoDialog extends StatelessWidget {
+  final CourseLesson lesson;
+
+  const _LessonInfoDialog({
+    Key? key,
+    required this.lesson,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final startTimeString = '${lesson.startDateTime.hour}:'
+        '${lesson.startDateTime.minute}';
+    final endTimeString = '${lesson.endDateTime.hour}:'
+        '${lesson.endDateTime.minute}';
+
+    return Center(
+      child: Hero(
+        tag: lesson,
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+          child: SizedBox(
+            height: 480,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              child: IconTheme(
+                data: IconThemeData(
+                  size: 18,
+                  color: Theme.of(context).colorScheme.tertiary,
+                ),
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    fontSize: 14.5,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          lesson.course!.name,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          maxLines: 3,
+                        ),
+                        const SizedBox(height: 30),
+                        Row(
+                          children: [
+                            const Icon(Icons.access_time_rounded),
+                            const SizedBox(width: 8),
+                            Text('$startTimeString '),
+                            Icon(Icons.arrow_forward_rounded,
+                              color: Theme.of(context).colorScheme.onSecondaryContainer,
+                            ),
+                            Text(' $endTimeString'),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            const Icon(Icons.place_outlined),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: Text(
+                                '${lesson.room}, ${lesson.building}',
+                                maxLines: 3,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    maxLines: 2,
                   ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      const Icon(Icons.access_time_rounded),
-                      const SizedBox(width: 8),
-                      Text('$startTimeString '),
-                      Icon(Icons.arrow_forward_rounded,
-                        color: Theme.of(context).colorScheme.onSecondaryContainer,
-                      ),
-                      Text(' $endTimeString'),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      const Icon(Icons.place_outlined),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Text('${lesson.room}, ${lesson.building}'),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
           ),
